ec4322aee661773953fd3188d29ccfd59e018e43
filipw <wojcieszyn@gmail.com>
2023-05-09 21:38:20 +0200

    new QIR runner path

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-../collapse/src/Collapse/bin/Release/net6.0/osx-arm64/publish/Collapse sim ./1-hadamard/ --shots 250 --qir
+../collapse/src/Collapse/bin/Release/net6.0/publish/Collapse sim ./1-hadamard/ --shots 250 --qir --qir-runner ~/opt/qir-runner.0.3.0/qir-runner
--- 3.sh
+++ 3.sh
@@ -1,1 +1,1 @@
-dotnet run --project ../collapse/src/Collapse sim ./3-entanglement/ --shots 250 --qir
+../collapse/src/Collapse/bin/Release/net6.0/publish/Collapse sim ./3-entanglement/ --shots 250 --qir --qir-runner ~/opt/qir-runner.0.3.0/qir-runner
